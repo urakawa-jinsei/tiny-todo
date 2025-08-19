84d04daa9d986a0014383f879224cedf6c4b489d
urakawa-jinsei <urakawa_jinsei@retail-ai.jp>
2025-08-19 11:00:52 +0900

    [add]セッションを追加

--- main.go
+++ main.go
@@ -1,28 +1,11 @@
 package main
 
 import (
-	"html/template"
 	"log"
 	"net/http"
 )
 
-var todoList []string
-
-func handleTodo(w http.ResponseWriter, r *http.Request) {
-	t, _ := template.ParseFiles("templates/todo.html")
-	t.Execute(w, todoList)
-}
-
-func handleAdd(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()
-	todo := r.Form.Get("todo")
-	todoList = append(todoList, todo)
-	http.Redirect(w, r, "/todo", http.StatusSeeOther)
-}
-
 func main() {
-	todoList = append(todoList, "顔を洗う", "朝食を食べる", "歯を磨く")
-
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
 	http.HandleFunc("/todo", handleTodo)
